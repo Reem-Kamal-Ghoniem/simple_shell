259088dee2e12b24286e04d2deb2d3c6653d5351
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-05-24 11:50:04 -0700

    removed

--- shell.c
+++ shell.c
@@ -69,7 +69,6 @@ int main(__attribute__((unused))int ac, char **av, char **env)
 			write(1, "#cisfun$ ", 9);
 		if (getline(&s, &size, stdin) == -1)
 		{
-			write(1, "\n", 1);
 			exit(0);
 		}
 		argv = command_line(s), exit_status(argv);
