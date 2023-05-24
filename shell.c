2738da3810d5382930c0df94c896ab4e683acfa8
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-05-24 13:02:41 -0700

    new main

--- shell.c
+++ shell.c
@@ -30,7 +30,10 @@ void non_interactive(char **av, char **env)
 	argv = command_line(s);
 	exit_status(argv);
 	if (!(argv[0]) || environment(argv) || _cd(av[0], argv, env))
-		return;;
+	{
+		free_argv(argv);
+		return;
+	}
 	t = _path(argv[0], env);
 	if (t)
 		argv[0] = t;
@@ -54,9 +57,9 @@ int main(__attribute__((unused))int ac, char **av, char **env)
 	pid_t id;
 /*	struct stat file;*/
 
-/*	if (!isatty(STDIN_FILENO))
-		non_interactive(av, env);*/
-	while (1)
+	if (!isatty(STDIN_FILENO))
+		non_interactive(av, env);
+	while (isatty(STDIN_FILENO))
 	{
 /*		if (av[1])
 		{
@@ -65,8 +68,8 @@ int main(__attribute__((unused))int ac, char **av, char **env)
 				perror(av[0]), exit(1);
 		}
 		else*/
-		if (isatty(STDIN_FILENO))
-			write(1, "#cisfun$ ", 9);
+		/**if (isatty(STDIN_FILENO))*/
+		write(1, "#cisfun$ ", 9);
 		if (getline(&s, &size, stdin) == -1)
 			exit(0);
 		argv = command_line(s), exit_status(argv);
